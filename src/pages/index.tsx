0b093eebc82bd47894acff4fce4fb620df1483be
Ryo Takeuchi <develop@r09i.me>
2022-07-02 08:31:16 +0900

    :art: Comming Soon の文言を追加
    Comming Soon の文言を追加しました。

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -56,21 +56,13 @@ const Home: NextPage = () => {
 
           <section className="flex flex-col items-center">
             <div className="max-w-xl flex flex-col items-center text-center pt-8 lg:pt-32 pb-16 lg:pb-48">
-              <p className="text-indigo-500 md:text-lg xl:text-xl font-semibold mb-4 md:mb-6">Very proud to
-                introduce</p>
-
               <h1 className="text-black-800 text-4xl sm:text-5xl md:text-6xl font-bold mb-8 md:mb-12"
               >Puentet</h1>
 
-              <div className="w-full flex flex-col sm:flex-row sm:justify-center gap-2.5">
-                <a href="#"
-                   className="inline-block bg-indigo-500 hover:bg-indigo-600 active:bg-indigo-700 focus-visible:ring ring-indigo-300 text-white text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-3">Start
-                  now</a>
+              <p
+                className="text-indigo-500 md:text-lg xl:text-xl font-semibold mb-4 md:mb-6"
+              >Comming Soon</p>
 
-                <a href="#"
-                   className="inline-block bg-gray-200 hover:bg-gray-300 focus-visible:ring ring-indigo-300 text-gray-500 active:text-gray-700 text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-3">Take
-                  tour</a>
-              </div>
             </div>
 
             <div className="flex justify-center lg:justify-start items-center gap-4">
