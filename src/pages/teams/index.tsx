476fb4c8968435fe29273883650fa4f734390d17
Ryo Takeuchi <develop@r09i.me>
2022-07-03 20:04:14 +0900

    :sparkles: チームページを追加
    チームページを追加しました。

---
+++ src/pages/teams/index.tsx
@@ -0,0 +1,18 @@
+import { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+const TeamPage: NextPage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace('/');
+  }, []);
+
+  return (
+    <main>
+    </main>
+  );
+};
+
+export default TeamPage;
