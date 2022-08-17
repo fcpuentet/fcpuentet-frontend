a62009bd913bfc2f2b4fd7a5f5886e21ec7f0c76
Ryo Takeuchi <develop@r09i.me>
2022-08-17 15:09:08 +0900

    :sparkles: MyDocument を定義
    サイト全体に反映させたいデフォルトの設定用で MyDocument を定義しました。

---
+++ src/pages/_document.tsx
@@ -0,0 +1,35 @@
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return initialProps;
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html
+        lang='ja'
+        dir='ltr'
+      >
+        <Head>
+          {/* サイト全体に反映させたいデフォルトの設定を記述する ex) Google Fonts の読み込み */}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
