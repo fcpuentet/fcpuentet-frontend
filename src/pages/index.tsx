eeec43b49a1185206fc53e234897626c708ba9af
Ryo Takeuchi <develop@r09i.me>
2022-08-17 15:52:36 +0900

    :sparkles: Twitter リンクを追加
    Twitter のリンクを追加しました。

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -53,24 +53,7 @@ const TopPage: NextPage = () => {
                 <span className='h-px w-12 bg-gray-200'></span>
 
                 <div className='flex gap-4'>
-                  <a
-                    href='#'
-                    target='_blank'
-                    className='text-gray-400 transition duration-100 hover:text-gray-500 active:text-gray-600'
-                  >
-                    <svg
-                      className='h-5 w-5'
-                      width='24'
-                      height='24'
-                      viewBox='0 0 24 24'
-                      fill='currentColor'
-                      xmlns='http://www.w3.org/2000/svg'
-                    >
-                      <path d='M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z'></path>
-                    </svg>
-                  </a>
-
-                  <Link href='https://instagram.com/fcpuentet?igshid=YmMyMTA2M2Y='>
+                  <Link href='https://instagram.com/fcpuentet'>
                     <a
                       target='_blank'
                       className='text-gray-400 transition duration-100 hover:text-gray-500 active:text-gray-600'
@@ -88,22 +71,23 @@ const TopPage: NextPage = () => {
                     </a>
                   </Link>
 
-                  <a
-                    href='#'
-                    target='_blank'
-                    className='text-gray-400 transition duration-100 hover:text-gray-500 active:text-gray-600'
-                  >
-                    <svg
-                      className='h-5 w-5'
-                      width='24'
-                      height='24'
-                      viewBox='0 0 24 24'
-                      fill='currentColor'
-                      xmlns='http://www.w3.org/2000/svg'
+                  <Link href='https://twitter.com/FCPUENTET'>
+                    <a
+                      target='_blank'
+                      className='text-gray-400 transition duration-100 hover:text-gray-500 active:text-gray-600'
                     >
-                      <path d='M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z' />
-                    </svg>
-                  </a>
+                      <svg
+                        className='h-5 w-5'
+                        width='24'
+                        height='24'
+                        viewBox='0 0 24 24'
+                        fill='currentColor'
+                        xmlns='http://www.w3.org/2000/svg'
+                      >
+                        <path d='M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z' />
+                      </svg>
+                    </a>
+                  </Link>
                 </div>
               </div>
             </section>
