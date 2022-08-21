81b7e8441b09701340491e9fd8966eb04c2a8db9
Ryo Takeuchi <develop@r09i.me>
2022-08-21 11:57:21 +0900

    :sparkles: トップ画面のお知らせ取得方法を変更 (#16)
    トップ画面のお知らせ取得方法を変更しました。

--- src/features/top/components/TopFeeds.tsx
+++ src/features/top/components/TopFeeds.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import { TopContentLayout, TopFeedContent } from '@/features/top';
+import { useTopFeeds } from '@/features/top';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface Props {}
 
 export const TopFeeds: React.FC<Props> = () => {
+  const { feeds } = useTopFeeds();
+  const feedItems = feeds?.map((feed) => (
+    <TopFeedContent
+      key={feed.title}
+      title={feed.title}
+      body={feed.body}
+    />
+  ));
+
   return (
     <TopContentLayout>
       <section className='flex flex-col'>
         <h2 className='cursor-default text-7xl'>NEWS</h2>
         <div>
-          <ul className='p-8'>
-            <TopFeedContent title='NEWS 1' />
-            <TopFeedContent title='NEWS 2' />
-            <TopFeedContent title='NEWS 3' />
-            <TopFeedContent title='NEWS 4' />
-          </ul>
+          <ul className='p-8'>{feedItems}</ul>
         </div>
       </section>
     </TopContentLayout>
