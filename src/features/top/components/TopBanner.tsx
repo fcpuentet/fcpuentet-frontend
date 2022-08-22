2927a41666972344466aac693a09a4dd0166e40a
Ryo Takeuchi <develop@r09i.me>
2022-08-23 08:51:38 +0900

    :art: トップバナーのスタイル調整
    トップバナーのスタイルを調整しました。

--- src/features/top/components/TopBanner.tsx
+++ src/features/top/components/TopBanner.tsx
@@ -5,16 +5,15 @@ interface Props {}
 
 export const TopBanner: React.FC<Props> = () => {
   return (
-    <div className='w-full'>
-      <section className=''>
-        <h1>
-          <img
-            src='/top-banner.webp'
-            className='object-contain'
-            alt='FC-PUENTET'
-          />
-        </h1>
-      </section>
-    </div>
+    <section className='w-full'>
+      <h1>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          src='/top-banner.webp'
+          className='select-none object-contain'
+          alt='FC-PUENTET'
+        />
+      </h1>
+    </section>
   );
 };
