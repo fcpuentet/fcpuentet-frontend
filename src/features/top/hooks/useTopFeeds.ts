930d53377911e0c936c6d4b83e1ff93a23ab5151
Ryo Takeuchi <develop@r09i.me>
2022-08-23 21:50:18 +0900

    :sparkles: お知らせをデータベースから取得するよう変更 (#21)
    お知らせをデータベースから取得するよう実装しました。

--- src/features/top/hooks/useTopFeeds.ts
+++ src/features/top/hooks/useTopFeeds.ts
@@ -1,3 +1,4 @@
+import { supabaseClient } from '@supabase/auth-helpers-nextjs';
 import { useEffect, useState } from 'react';
 import { Feed } from '@/features/feed';
 
@@ -5,17 +6,47 @@ type TopNews = {
   feeds?: Feed[];
 };
 
+const TOP_FEED_MAX_COUNT = 3;
+
 export const useTopFeeds = (): TopNews => {
   const [feeds, setFeeds] = useState<Feed[]>();
 
   useEffect(() => {
-    const feeds = Array.from<void, Feed>({ length: 3 }, (v, k) => {
-      return {
-        title: `NEWS ${k + 1}`,
-        body: 'Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.',
-      };
-    });
-    setFeeds(feeds);
+    void (async function () {
+      try {
+        const { data: feedList, error } = await supabaseClient
+          .from('feeds')
+          .select('id, created_at, updated_at, title, body')
+          .is('deleted_at', null)
+          .order('created_at', { ascending: false })
+          .limit(TOP_FEED_MAX_COUNT);
+
+        if (error) {
+          return;
+        }
+
+        const feeds = feedList?.map<Feed>((value) => {
+          return {
+            // eslint-disable-next-line @typescript-eslint/restrict-template-expressions,@typescript-eslint/no-unsafe-member-access
+            id: `${value.id}`,
+            // eslint-disable-next-line @typescript-eslint/restrict-template-expressions,@typescript-eslint/no-unsafe-member-access
+            createdAt: `${value.created_at}`,
+            // eslint-disable-next-line @typescript-eslint/restrict-template-expressions,@typescript-eslint/no-unsafe-member-access
+            updatedAt: `${value.updated_at}`,
+            // eslint-disable-next-line @typescript-eslint/restrict-template-expressions,@typescript-eslint/no-unsafe-member-access
+            title: `${value.title}`,
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,@typescript-eslint/restrict-template-expressions
+            body: `${value.body}`,
+          };
+        });
+
+        setFeeds(feeds);
+        // eslint-disable-next-line no-empty
+      } catch {
+        // eslint-disable-next-line no-empty
+      } finally {
+      }
+    })();
   }, []);
 
   return {
