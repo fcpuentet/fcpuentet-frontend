862febbab7b6606b4e4f0583b0c374102e0b346d
Ryo Takeuchi <develop@r09i.me>
2022-08-25 07:27:14 +0900

    :sparkles: トップ画面のお知らせ項目に一覧への画面遷移ボタンを設置 (#29)
    トップ画面のお知らせ項目に一覧への画面遷移ボタンを設置しました。

--- src/features/top/components/TopFeeds.tsx
+++ src/features/top/components/TopFeeds.tsx
@@ -25,6 +25,21 @@ export const TopFeeds: React.FC<Props> = () => {
           <ul className='p-8'>{feedItems}</ul>
         </div>
       </section>
+      <div className='flex justify-end'>
+        <button
+          className='flex select-none flex-row items-center gap-4 px-8 text-2xl'
+          onClick={() => void push(`/feeds`)}
+        >
+          もっとみる
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            height='40'
+            width='40'
+          >
+            <path d='m20 33.333-1.958-1.958 9.958-10H6.667v-2.75H28l-9.958-10L20 6.667 33.333 20Z' />
+          </svg>
+        </button>
+      </div>
     </TopContentLayout>
   );
 };
