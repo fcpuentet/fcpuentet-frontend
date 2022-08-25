45699268626977f5909ce399c975dc18482981b0
Ryo Takeuchi <develop@r09i.me>
2022-08-25 23:20:25 +0900

    :sparkles: お問い合わせページを追加 (#32)
    お問い合わせページを追加しました。

---
+++ src/features/contact/components/ContactHeader.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link';
+import React from 'react';
+
+type Props = React.HTMLAttributes<HTMLDivElement>;
+
+export const ContactHeader: React.FC<Props> = ({ ...divHTMLAttributes }: Props): JSX.Element => {
+  return (
+    <div
+      {...divHTMLAttributes}
+      className='flex flex-col lg:flex-row-reverse lg:items-center'
+    >
+      <Link href='/'>
+        <a>
+          <img
+            src='/teamname.svg'
+            className='h-8 max-w-fit select-none lg:h-12'
+          />
+        </a>
+      </Link>
+      <h2 className='mt-8 flex-1 cursor-default select-none text-4xl lg:mt-0 lg:text-7xl'>
+        Contact
+      </h2>
+    </div>
+  );
+};
---
+++ src/features/contact/hooks/useContact.ts
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+
+type FeedState = {
+  name: string;
+  onChangeName: (value: string) => void;
+  email: string;
+  onChangeEmail: (value: string) => void;
+  content: string;
+  onChangeContent: (value: string) => void;
+  onSubmit: () => void;
+};
+
+export const useContact = (initEmail?: string, initBody?: string): FeedState => {
+  const [name, setName] = useState('');
+  const onChangeName = (value: string) => {
+    setName(value);
+  };
+
+  const [email, setEmail] = useState(initEmail ?? '');
+  const onChangeEmail = (value: string) => {
+    setEmail(value);
+  };
+
+  const [content, setContent] = useState(initBody ?? '');
+  const onChangeContent = (value: string) => {
+    setContent(value);
+  };
+
+  const onSubmit = () => {
+    // TODO: implementation
+    // void (async function () {
+    //   await supabaseClient.from('contact').insert({
+    //     name,
+    //     email,
+    //     content,
+    //   });
+    // })();
+  };
+
+  return {
+    name,
+    onChangeName,
+    email,
+    onChangeEmail,
+    content,
+    onChangeContent,
+    onSubmit,
+  };
+};
