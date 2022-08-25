b666143be5ec496053c85fc90089049639a4a48f
Ryo Takeuchi <develop@r09i.me>
2022-08-25 21:24:27 +0900

    :sparkles: メニューを追加 (#30)
    メニューを追加しました。

--- src/components/Elements/Header/Header.tsx
+++ src/components/Elements/Header/Header.tsx
@@ -1,73 +1,63 @@
-import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+import { ContentLayout } from '@/components/Layout';
 
-export const Header = () => {
+interface MenuItem {
+  name: string;
+  path: string;
+}
+
+const menuItems: MenuItem[] = [
+  {
+    name: 'Top',
+    path: '/',
+  },
+  {
+    name: 'News',
+    path: '/feeds',
+  },
+  {
+    name: 'Team',
+    path: '/teams',
+  },
+  {
+    name: 'Sponsor',
+    path: '/club/sponsors',
+  },
+  {
+    name: 'Contact',
+    path: '/contact',
+  },
+];
+
+export const Header = (): JSX.Element => {
+  const menuItemElements = menuItems.map((menuItem) => (
+    <Link
+      key={menuItem.name}
+      href={menuItem.path}
+    >
+      <a className='text-4xl font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700'>
+        {menuItem.name}
+      </a>
+    </Link>
+  ));
+
   return (
-    <>
-      <header className='mb-8 flex items-center justify-between border-b py-4 md:mb-12 md:py-8 xl:mb-16'>
-        <Link href='/'>
-          <a
-            className='text-black-800 inline-flex items-center gap-2.5 text-2xl font-bold md:text-3xl'
-            aria-label='logo'
-          >
-            <Image
-              src='/emblem.webp'
-              width='108'
-              height='64'
-            />
-          </a>
-        </Link>
-
-        <nav className='hidden gap-12 lg:flex'>
+    <ContentLayout className='block min-h-screen md:grid md:place-content-center'>
+      <header className='grid grid-cols-1 md:grid-cols-2'>
+        <div className='flex items-center justify-center'>
           <Link href='/'>
-            <a className='text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700'>
-              Home
+            <a aria-label='logo'>
+              <img
+                src='/emblem.webp'
+                className='mx-auto w-3/4 md:w-full'
+              />
             </a>
           </Link>
-          <Link href='/teams/members'>
-            <a className='text-lg font-semibold text-indigo-500'>Members</a>
-          </Link>
-          <a
-            href='#'
-            className='text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700'
-          >
-            Pricing
-          </a>
-          <a
-            href='#'
-            className='text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700'
-          >
-            About
-          </a>
-        </nav>
+        </div>
 
-        <a
-          href='#'
-          className='hidden rounded-lg bg-gray-200 px-8 py-3 text-center text-sm font-semibold text-gray-500 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-300 focus-visible:ring active:text-gray-700 md:text-base lg:inline-block'
-        >
-          Contact Sales
-        </a>
-
-        <button
-          type='button'
-          className='inline-flex items-center gap-2 rounded-lg bg-gray-200 px-2.5 py-2 text-sm font-semibold text-gray-500 ring-indigo-300 hover:bg-gray-300 focus-visible:ring active:text-gray-700 md:text-base lg:hidden'
-        >
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            className='h-6 w-6'
-            viewBox='0 0 20 20'
-            fill='currentColor'
-          >
-            <path
-              fillRule='evenodd'
-              d='M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z'
-              clipRule='evenodd'
-            />
-          </svg>
-          Menu
-        </button>
+        <nav className='grid gap-8 px-8 py-16 md:place-content-center'>{menuItemElements}</nav>
       </header>
-    </>
+    </ContentLayout>
   );
 };
--- src/components/Layout/MainLayout.tsx
+++ src/components/Layout/MainLayout.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import React, { useState } from 'react';
-import { Footer } from '../Elements';
+import { Footer, Header } from '@/components/Elements';
 
 interface Props {
   title?: string;
@@ -115,7 +115,21 @@ export const MainLayout: React.FC<Props> = ({ title, children }: Props) => {
                 isMenuVisible ? 'visible' : 'invisible'
               }`}
             >
-              {/*<Header />*/}
+              <div className='block text-right lg:hidden'>
+                <button
+                  className='select-none p-4'
+                  onClick={(event) => onToggleMenuVisibleButtonClick()}
+                >
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    height='48'
+                    width='48'
+                  >
+                    <path d='m12.45 37.65-2.1-2.1L21.9 24 10.35 12.45l2.1-2.1L24 21.9l11.55-11.55 2.1 2.1L26.1 24l11.55 11.55-2.1 2.1L24 26.1Z' />
+                  </svg>
+                </button>
+              </div>
+              <Header />
             </div>
 
             <div>
