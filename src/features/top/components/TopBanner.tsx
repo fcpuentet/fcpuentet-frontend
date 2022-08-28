d3b4b42ac4f398f019db46a46205265206c0b7e9
Ryo Takeuchi <develop@r09i.me>
2022-08-28 22:14:47 +0900

    :art: トップバナーを差し替え (#42)
    トップバナーを差し替えました。

--- src/features/top/components/TopBanner.tsx
+++ src/features/top/components/TopBanner.tsx
@@ -7,12 +7,22 @@ export const TopBanner: React.FC<Props> = () => {
   return (
     <section className='w-full'>
       <h1>
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img
-          src='/top-banner.webp'
-          className='select-none object-contain'
-          alt='FC-PUENTET'
-        />
+        <div className='block md:hidden'>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            src='/top-banner.jpg'
+            className='select-none object-contain'
+            alt='FC-PUENTET'
+          />
+        </div>
+        <div className='hidden md:block'>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            src='/top-banner-md.jpg'
+            className='select-none object-contain'
+            alt='FC-PUENTET'
+          />
+        </div>
       </h1>
     </section>
   );
