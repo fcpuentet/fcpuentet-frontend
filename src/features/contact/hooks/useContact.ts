085f693e0e46ef463a7efe481f222ee5cdcc3feb
Ryo Takeuchi <develop@r09i.me>
2022-08-28 22:01:36 +0900

    :emoji: お問い合わせカテゴリを追加
    お問い合わせカテゴリを追加しました。

--- src/features/contact/hooks/useContact.ts
+++ src/features/contact/hooks/useContact.ts
@@ -1,7 +1,11 @@
 import { supabaseClient } from '@supabase/auth-helpers-nextjs';
-import { useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
+import { ContactCategory, ContactTemplate } from '@/features/contact';
 
-type FeedState = {
+type ContactState = {
+  categories?: ContactCategory[];
+  category?: ContactCategory;
+  onChangeCategory: (categoryId: string) => void;
   name: string;
   onChangeName: (value: string) => void;
   email: string;
@@ -11,21 +15,145 @@ type FeedState = {
   onSubmit: () => void;
 };
 
-export const useContact = (initEmail?: string, initBody?: string): FeedState => {
+export const useContact = (initCategoryId?: string | string[]): ContactState => {
+  const [templates, setTemplates] = useState<ContactTemplate[]>();
+  const [categories, setCategories] = useState<ContactCategory[]>();
+  const [category, setCategory] = useState<ContactCategory>();
+  const onChangeCategory = (categoryId: string) => {
+    const selectedCategory = categories?.find((category) => category.id === categoryId);
+    setCategory(selectedCategory);
+
+    if (!selectedCategory?.templateId) {
+      return;
+    }
+    const template = templates?.find((template) => template.id === selectedCategory.templateId);
+    if (template) {
+      setContent(template.content);
+    }
+  };
+
   const [name, setName] = useState('');
   const onChangeName = (value: string) => {
     setName(value);
   };
 
-  const [email, setEmail] = useState(initEmail ?? '');
+  const [email, setEmail] = useState('');
   const onChangeEmail = (value: string) => {
     setEmail(value);
   };
 
-  const [content, setContent] = useState(initBody ?? '');
+  const [content, setContent] = useState('');
   const onChangeContent = (value: string) => {
     setContent(value);
   };
+
+  const fetchTemplates = async () => {
+    const { data: templateList, error } = await supabaseClient
+      .from('contact_templates')
+      .select('id, content')
+      .is('deleted_at', null);
+
+    if (error) {
+      return {
+        templates: undefined,
+        error,
+      };
+    }
+
+    const templates = templateList?.map<ContactTemplate>((value) => {
+      return {
+        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions,@typescript-eslint/no-unsafe-member-access
+        id: `${value.id}`,
+        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions,@typescript-eslint/no-unsafe-member-access
+        content: `${value.content}`,
+      };
+    });
+
+    return {
+      templates,
+      error: null,
+    };
+  };
+
+  const fetchCategories = async () => {
+    const { data: categoryList, error } = await supabaseClient
+      .from('contact_categories')
+      .select('id, name, template_id')
+      .is('deleted_at', null)
+      .order('sort', { ascending: true })
+      .order('created_at', { ascending: false });
+
+    if (error) {
+      return {
+        categories: undefined,
+        error,
+      };
+    }
+
+    const categories = categoryList?.map<ContactCategory>((value) => {
+      return {
+        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions,@typescript-eslint/no-unsafe-member-access
+        id: `${value.id}`,
+        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions,@typescript-eslint/no-unsafe-member-access
+        name: `${value.name}`,
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/restrict-template-expressions,@typescript-eslint/no-unsafe-member-access
+        templateId: `${value.template_id}`,
+      };
+    });
+
+    return {
+      categories,
+      error: null,
+    };
+  };
+
+  useMemo(() => {
+    void (async function () {
+      const { templates, error: templateError } = await fetchTemplates();
+      if (templateError) {
+        return;
+      }
+      setTemplates(templates);
+
+      const { categories, error: categoriesError } = await fetchCategories();
+      if (categoriesError) {
+        return;
+      }
+      setCategories(categories);
+      if (categories === undefined || categories.length === 0) {
+        return;
+      }
+
+      const firstCategory =
+        categories.find((category) => category.id === initCategoryId) ?? categories[0];
+      setCategory(firstCategory);
+
+      if (!firstCategory.templateId) {
+        return;
+      }
+      const template = templates?.find((template) => template.id === firstCategory.templateId);
+      if (template) {
+        setContent(template.content);
+      }
+    })();
+  }, []);
+
+  useCallback(() => {
+    if (!categories) {
+      return;
+    }
+
+    const firstCategory = categories.find((category) => category.id === initCategoryId);
+    setCategory(firstCategory);
+
+    if (!firstCategory?.templateId) {
+      return;
+    }
+    const template = templates?.find((template) => template.id === firstCategory.templateId);
+    if (template) {
+      setContent(template.content);
+    }
+  }, [initCategoryId]);
 
   const reset = () => {
     setName('');
@@ -49,6 +177,9 @@ export const useContact = (initEmail?: string, initBody?: string): FeedState => 
   };
 
   return {
+    categories,
+    category,
+    onChangeCategory,
     name,
     onChangeName,
     email,
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,15 +1,20 @@
 import type { NextPage } from 'next';
+import { useRouter } from 'next/router';
 import React from 'react';
 import { ContentLayout, MainLayout } from '@/components/Layout';
 import { ContactForm, ContactHeader } from '@/features/contact';
 
 const ContactScreen: NextPage = () => {
+  const {
+    query: { category },
+  } = useRouter();
+
   return (
     <MainLayout>
       <ContentLayout className='py-8 lg:pt-16'>
         <ContactHeader />
         <div className='pt-12 lg:px-8 lg:pb-8 lg:pt-24'>
-          <ContactForm />
+          <ContactForm category={category} />
         </div>
       </ContentLayout>
     </MainLayout>
