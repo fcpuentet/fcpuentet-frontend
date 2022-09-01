6302ed50ebcda26f99e9cbfd601e535143fb38a7
Ryo Takeuchi <develop@r09i.me>
2022-09-01 20:36:09 +0900

    :horse: next-sitemap の config 設定値を変更
    next-sitemap の config 設定値を変更しました。

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,24 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: process.env.SITE_URL || 'https://fc-puentet.com',
+  outDir: './out', // SSGだとビルド後にoutディレクトリができるので合わせる
   generateRobotsTxt: true,
+  transform: async (config, path) => {
+    if (isIgnorePath(path)) {
+      return null; // nullを返すとサイトマップから除外できる
+    }
+
+    return {
+      loc: encodeURI(path), // エンコードしたURLを返す
+      lastmod: customLastmod(path), // パスを元に、ページ毎の最終更新日を返す
+    };
+  },
+};
+
+const isIgnorePath = (path) => {
+  return false;
+};
+
+const customLastmod = (path) => {
+  return '2022-09-01';
 };
