04868cec390942d9393b5ad1e9d2eadeff74cab8
Ryo Takeuchi <develop@r09i.me>
2022-09-21 23:04:18 +0900

    :horse: router を Link コンポーネントに置き換え
    router を Link コンポーネントに置き換えし、デフォルトでプリフェッチするように変更しました。

--- src/features/feed/components/FeedListContent.tsx
+++ src/features/feed/components/FeedListContent.tsx
@@ -1,25 +1,24 @@
+import Link from 'next/link';
 import React from 'react';
 import { News } from '@/features/feed';
 
-interface Props {
+interface FeedListContentProps {
   news: News;
-  onClick: (id: string) => void;
 }
 
-export const FeedListContent: React.FC<Props> = ({ news, onClick }: Props) => {
+export const FeedListContent: React.FC<FeedListContentProps> = ({ news }: FeedListContentProps) => {
   return (
-    <li
-      className='my-6 block cursor-pointer select-none rounded-lg border border-gray-200 bg-white p-6 shadow-md hover:bg-gray-100'
-      onClick={() => onClick(news.id)}
-    >
-      <a className='flex flex-col justify-between md:flex-row md:items-center'>
-        <h5 className='mb-2 text-xl font-bold tracking-tight text-gray-900 lg:text-2xl'>
-          {news.title}
-        </h5>
-        <p className='text-end font-normal text-gray-700'>
-          更新日: {new Date(news.updatedAtString).toLocaleDateString()}
-        </p>
-      </a>
+    <li className='my-6 block cursor-pointer select-none rounded-lg border border-gray-200 bg-white p-6 shadow-md hover:bg-gray-100'>
+      <Link href={`/feeds/${news.id}`}>
+        <a className='flex flex-col justify-between md:flex-row md:items-center'>
+          <h5 className='mb-2 text-xl font-bold tracking-tight text-gray-900 lg:text-2xl'>
+            {news.title}
+          </h5>
+          <p className='text-end font-normal text-gray-700'>
+            更新日: {new Date(news.updatedAtString).toLocaleDateString()}
+          </p>
+        </a>
+      </Link>
     </li>
   );
 };
--- src/features/top/components/TopFeeds.tsx
+++ src/features/top/components/TopFeeds.tsx
@@ -1,4 +1,4 @@
-import { useRouter } from 'next/router';
+import Link from 'next/link';
 import React from 'react';
 import { FeedListContent, News } from '@/features/feed';
 import { TopContentLayout } from '@/features/top';
@@ -9,12 +9,10 @@ interface TopFeedsProps {
 }
 
 export const TopFeeds: React.FC<TopFeedsProps> = ({ newsList }) => {
-  const { push } = useRouter();
   const feedItems = newsList?.map((news) => (
     <FeedListContent
       key={news.id}
       news={news}
-      onClick={() => void push(`/feeds/${news.id}`)}
     />
   ));
 
@@ -29,19 +27,18 @@ export const TopFeeds: React.FC<TopFeedsProps> = ({ newsList }) => {
         </div>
       </section>
       <div className='flex justify-end'>
-        <button
-          className='flex select-none flex-row items-center gap-4 text-xl lg:px-8 lg:text-2xl'
-          onClick={() => void push(`/feeds`)}
-        >
-          もっとみる
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            height='40'
-            width='40'
-          >
-            <path d='m20 33.333-1.958-1.958 9.958-10H6.667v-2.75H28l-9.958-10L20 6.667 33.333 20Z' />
-          </svg>
-        </button>
+        <Link href='/feeds'>
+          <a className='flex select-none flex-row items-center gap-4 text-xl lg:px-8 lg:text-2xl'>
+            もっとみる
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              height='40'
+              width='40'
+            >
+              <path d='m20 33.333-1.958-1.958 9.958-10H6.667v-2.75H28l-9.958-10L20 6.667 33.333 20Z' />
+            </svg>
+          </a>
+        </Link>
       </div>
     </TopContentLayout>
   );
