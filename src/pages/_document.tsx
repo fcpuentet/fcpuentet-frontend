4172244d139184992792df18adea76e8cbd4a452
Ryo Takeuchi <develop@r09i.me>
2022-09-27 21:50:29 +0900

    :art: ファビコンを変更
    ファビコン等を変更しました。
    
    https://qiita.com/purpleeeee/items/cd9aca1ae735ad678355

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -39,6 +39,44 @@ class MyDocument extends Document {
             href='https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap'
             rel='stylesheet'
           />
+          <link
+            rel='apple-touch-icon'
+            sizes='180x180'
+            href='/favicons/apple-touch-icon.png'
+          />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='32x32'
+            href='/favicons/favicon-32x32.png'
+          />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='16x16'
+            href='/favicons/favicon-16x16.png'
+          />
+          <link
+            rel='manifest'
+            href='/favicons/site.webmanifest'
+          />
+          <link
+            rel='mask-icon'
+            href='/favicons/safari-pinned-tab.svg'
+            color='#000000'
+          />
+          <meta
+            name='msapplication-TileColor'
+            content='#ffffff'
+          />
+          <meta
+            name='msapplication-config'
+            content='/favicons/browserconfig.xml'
+          />
+          <meta
+            name='theme-color'
+            content='#ffffff'
+          />
           <meta
             key='theme-color-light'
             name='theme-color'
