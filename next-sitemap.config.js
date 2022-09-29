1ea80c78e51c878846cd6167424a6e1247874f44
Ryo Takeuchi <develop@r09i.me>
2022-09-30 08:12:55 +0900

    Merge pull request #91
    * :sparkles: sitemap の公開

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: process.env.SITE_URL || 'https://fc-puentet.com',
-  outDir: './out', // SSGだとビルド後にoutディレクトリができるので合わせる
   generateRobotsTxt: true,
   transform: async (config, path) => {
     if (isIgnorePath(path)) {
