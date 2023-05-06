4564a9918ea95323d74fb330fe2e03679f09d967
ka10take <kato.development1618@gmail.com>
2023-05-06 21:37:23 +0900

    :+1: カードスタイル修正

--- apps/frontend/src/features/staffs/components/StaffsCard.tsx
+++ apps/frontend/src/features/staffs/components/StaffsCard.tsx
@@ -6,15 +6,15 @@ interface StaffsCardProps {
 
 export const StaffsCard: React.FC<StaffsCardProps> = ({ staff }) => {
   return (
-    <div className='w-full h-full overflow-hidden rounded-md relative shadow-md m-auto'>
+    <div className='w-full h-full overflow-hidden rounded-md relative shadow-md m-auto bg-secondary'>
       <img
         src={staff.image}
         alt={staff.name}
       />
 
-      <p className='text-white text-xl text-center w-full bg-secondary py-2'>
-        <span className='text-sm mr-2'>{staff.position}</span>
-        {staff.name}
+      <p className=' text-white text-xl text-center w-full p-2'>
+        <span className='text-sm mr-2 inline-block'>{staff.position}</span>
+        <span className='inline-block'>{staff.name}</span>
       </p>
     </div>
   );
