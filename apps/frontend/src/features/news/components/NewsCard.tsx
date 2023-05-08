1b7cede7c935f1564f4b11a902b9855fc6740d5f
ka10take <kato.development1618@gmail.com>
2023-05-08 22:05:57 +0900

    :+1: ESLintエラー修正

--- apps/frontend/src/features/news/components/NewsCard.tsx
+++ apps/frontend/src/features/news/components/NewsCard.tsx
@@ -1,7 +1,7 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import { News } from '../types';
 import { formatDate } from '@/utils';
-import Image from 'next/image';
 
 interface NewsCardProps {
   news: News;
